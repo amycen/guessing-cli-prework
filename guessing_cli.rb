a67b798f3b8f74f2509c85244a7c4fdb7f8a7f33
Amy Cen <amy.cen@blackrock.com>
2019-04-19 01:18:19 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,9 @@
 # Code your solution here!
+require "pry"
+def run_guessing_game
+  your_guess = nil
+  play = true
+  while play
+    puts "Guess an integer:"
+    your_guess = gets.chomp.to_i
+    binding.pry
